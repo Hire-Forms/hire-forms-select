82945ba0c1526981c9bc01ae757c39aa41707a16
Gijsjan <agijsbro@gmail.com>
2016-05-10 07:44:35 +0000

    Pass children to <Options />

--- src/index.jsx
+++ src/index.jsx
@@ -1,50 +1,49 @@
-import React from "react";
-import cx from "classnames";
-
-import Options from "hire-forms-options";
-import {stringOrKeyValueMap, arrayOfStringsOrArrayOfKeyValueMaps} from "hire-forms-prop-types";
-import {isKeyValueMap, isListOfStrings, castKeyValue, castKeyValueArray} from "hire-forms-utils";
+import React from 'react';
+import cx from 'classnames';
+import Options from 'hire-forms-options';
+import { stringOrKeyValueMap, arrayOfStringsOrArrayOfKeyValueMaps } from 'hire-forms-prop-types';
+import { isKeyValueMap, isListOfStrings, castKeyValue, castKeyValueArray } from 'hire-forms-utils';
 
 class Select extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.handleDocumentClick = this.handleDocumentClick.bind(this)
+		this.handleDocumentClick = this.handleDocumentClick.bind(this);
 
 		this.state = {
 			options: props.options,
-			visible: false
+			visible: false,
 		};
 	}
 
 	componentDidMount() {
-		document.addEventListener("click", this.handleDocumentClick, false);
+		document.addEventListener('click', this.handleDocumentClick, false);
 
 		if (this.props.async != null) {
 			this.props.async((response) => {
 				this.setState({
-					options: response
+					options: response,
 				});
 			});
 		}
 	}
 
 	componentWillReceiveProps(nextProps) {
-		if(nextProps.options && nextProps.options.length) {
+		if (nextProps.options && nextProps.options.length) {
 			this.setState({
-				options: nextProps.options
+				options: nextProps.options,
 			});
 		}
 	}
 
 	componentWillUnmount() {
-		document.removeEventListener("click", this.handleDocumentClick, false);
+		document.removeEventListener('click', this.handleDocumentClick, false);
 	}
 
 	handleDocumentClick(ev) {
 		if (this.state.visible && !this.refs.select.contains(ev.target)) {
 			this.setState({
-				visible: false
+				visible: false,
 			});
 		}
 	}
@@ -52,7 +51,7 @@ class Select extends React.Component {
 	handleInputClick(ev) {
 		// Visible state shouldn't change when there are no options.
 		if (this.state.options.length > 0) {
-			this.setState({visible: !this.state.visible});
+			this.setState({ visible: !this.state.visible });
 		}
 	}
 
@@ -61,7 +60,7 @@ class Select extends React.Component {
 	 * @param {object} value Map of key and value: {key: "somekey", value: "somevalue"}
 	 */
 	handleOptionsChange(value) {
-		this.setState({visible: false});
+		this.setState({ visible: false });
 
 		// If the options prop is an array of strings, return a string.
 		if (isListOfStrings(this.state.options)) {
@@ -72,26 +71,25 @@ class Select extends React.Component {
 	}
 
 	render() {
-		let optionValues, options;
-
-		if (this.state.visible) {
-			options = (
-				<Options
-					onChange={this.handleOptionsChange.bind(this)}
-					sort={this.props.sort}
-					sortRelevance={this.props.sortRelevance}
-					value={castKeyValue(this.props.value)}
-					values={castKeyValueArray(this.state.options)} />
-			);
-		}
+		const options = this.state.visible ?
+			<Options
+				onChange={this.handleOptionsChange.bind(this)}
+				sort={this.props.sort}
+				sortRelevance={this.props.sortRelevance}
+				value={castKeyValue(this.props.value)}
+				values={castKeyValueArray(this.state.options)}
+			>
+				{this.props.children}
+			</Options> :
+			null;
 
 		// If value prop is a key/value map, extract the value.
-		let value = isKeyValueMap(this.props.value) ?
+		const value = isKeyValueMap(this.props.value) ?
 			this.props.value.value :
 			this.props.value;
 
 		// Create new var so we can check value in cx()
-		let inputValue = (value === "") ?
+		const inputValue = (value === '') ?
 			this.props.placeholder :
 			value;
 
@@ -115,17 +113,18 @@ class Select extends React.Component {
 
 Select.defaultProps = {
 	options: [],
-	value: ""
+	value: '',
 };
 
 Select.propTypes = {
 	async: React.PropTypes.func,
+	children: React.PropTypes.node,
 	onChange: React.PropTypes.func.isRequired,
 	options: arrayOfStringsOrArrayOfKeyValueMaps,
 	placeholder: React.PropTypes.string,
 	sort: React.PropTypes.bool,
 	sortRelevance: React.PropTypes.bool,
-	value: stringOrKeyValueMap
+	value: stringOrKeyValueMap,
 };
 
 export default Select;
